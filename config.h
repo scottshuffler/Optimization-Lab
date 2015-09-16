a5839a1c3946bedbfb0bf3b66b0a3392820de2cd
Shuffler  Edward S <shuffleres@cs.cs.appstate.edu>
2015-09-16 16:06:16 -0400

    first commit

---
+++ config.h
@@ -0,0 +1,32 @@
+/*********************************************************
+ * config.h - Configuration data for the driver.c program.
+ *********************************************************/
+#ifndef _CONFIG_H_
+#define _CONFIG_H_
+
+/* 
+ * CPEs for the baseline (naive) version of the rotate function that
+ * was handed out to the students. Rd is the measured CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define R64    4.2
+#define R128   5.5
+#define R256   16.3
+#define R512   23.2
+#define R1024  77.7
+
+/* 
+ * CPEs for the baseline (naive) version of the smooth function that
+ * was handed out to the students. Sd is the measure CPE for a dxd
+ * image. Run the driver.c program on your system to get these
+ * numbers.  
+ */
+#define S32   226
+#define S64   227
+#define S128  229
+#define S256  230
+#define S512  236
+
+
+#endif /* _CONFIG_H_ */
